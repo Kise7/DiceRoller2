4291af01f5b043c37a2960ca3b1c0515b0332588
Kise7 <52307809+Kise7@users.noreply.github.com>
2019-10-26 21:59:04 +0800

    1.2 Basic app anatomy (Changes made)

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -15,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val countButton: Button = findViewById(R.id.countup_button)
+        countButton.setOnClickListener { countUp() }
+
     }
 
     private fun rollDice() {
@@ -28,5 +31,22 @@ class MainActivity : AppCompatActivity() {
 
         resultText.text = "Dice Rolled!"
         resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+
+        if (resultText.text == "Hello World!" ) {
+            resultText.text = "1"
+        } else {
+
+            var result = resultText.text.toString().toInt()
+
+            if (result < 6) {
+                result++
+                resultText.text = result.toString()
+            }
+        }
     }
 }
